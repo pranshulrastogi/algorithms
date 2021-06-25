8c91abac3208ca81e23a240ca3d53e199b2c4222
pranshul <pranshul.rs@gmail.com>
2021-06-25 22:53:17 +0530

    best_time_to_buy_and_sell_stock 2

---
+++ leetcode/best_time_to_buy_and_sell_stock_ii.py
@@ -0,0 +1,41 @@
+'''
+Problem Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+Difficulty: Easy
+Problem statement:
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e., max profit = 0.
+'''
+'''
+We will follow peak-valley approach, i.e., whenever any price is less thant the earlier price
+we sell and make profit
+'''
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i, ele in enumerate(prices[1:], start=1):
+            if ele-prices[i-1] > 0:
+                profit += ele-prices[i-1]
+        return profit
